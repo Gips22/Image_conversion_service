bfddd20d2c6df233cac92ea083bd0153a9271aa6
andrey <gipsme123@gmail.com>
2023-01-25 16:59:37 +0300

    Restructure, refactoring. Some problems with starting docker-compose remain

---
+++ enter/celery_config.py
@@ -0,0 +1,8 @@
+"""Модуль конфигурации celery"""
+from celery import Celery
+
+
+app_celery = Celery('tasks', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')  # создание ЭК Celery приложения с именем 'tasks' и настройкой брокера сообщений на Redis, который запущен на локальной машине по адресу localhost:6379/0.
+app_celery.conf.beat_schedule = {}  # эта настройка нужна если нужно запускать задачу с периодичностью (celery beat)
+
+
---
+++ enter/main.py
@@ -0,0 +1,30 @@
+import aiohttp_jinja2
+from aiohttp import web
+import jinja2
+
+
+routes = web.RouteTableDef()  # используется для создания и конфигурирования роутинга в aiohttp.
+app = web.Application()  # создание веб-сервера. Экземпляр приложения aiohttp, который может использоваться для обработки запросов и маршрутизации.
+                         # Этот экземпляр приложения может быть настроен с помощью различных мидлварей, роутов и т.д. и запущен с помощью функции run_app().
+
+
+def setup_app(app: web.Application) -> None:
+    """Настройка всего приложения"""
+    _setup_external_libraries(app)
+    _setup_routes(app)
+
+
+def _setup_routes(app: web.Application) -> None:
+    """Настройка роутов"""
+    from routes import setup_routes
+    setup_routes(app)
+
+
+def _setup_external_libraries(app: web.Application) -> None:
+    """Указываем шаблонизатору, что html-шаблоны надо искать в папке templates"""
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("templates"))
+
+
+if __name__ == "__main__":
+    setup_app(app)  # настройка приложения
+    web.run_app(app, host='0.0.0.0', port=8080)
